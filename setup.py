930b47b72cea624d25a9fbc792823a6ac6498f86
hpreston <hank.preston@gmail.com>
2018-11-10 20:55:24 -0500

    updated version to 0.1.1

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/hpreston/webexteamsbot',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
