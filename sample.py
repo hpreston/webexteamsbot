91e89bd4cd5010bc28901164c99b5ff31a6acab7
hpreston <hank.preston@gmail.com>
2018-11-11 21:34:03 -0500

    expanded comments and notes in sample bot

--- sample.py
+++ sample.py
@@ -13,6 +13,17 @@ teams_token = os.getenv("TEAMS_BOT_TOKEN")
 bot_url = os.getenv("TEAMS_BOT_URL")
 bot_app_name = os.getenv("TEAMS_BOT_APP_NAME")
 
+# Create a Bot Object
+#   Note: debug mode prints out more details about processing to terminal
+bot = TeamsBot(
+    bot_app_name,
+    teams_bot_token=teams_token,
+    teams_bot_url=bot_url,
+    teams_bot_email=bot_email,
+    debug=True,
+)
+
+
 # Create a custom bot greeting function returned when no command is given.
 # The default behavior of the bot is to return the '/help' command response
 def greeting(incoming_msg):
@@ -21,12 +32,15 @@ def greeting(incoming_msg):
 
     # Create a Response object and craft a reply in Markdown.
     response = Response()
-    response.markdown = "Hello {nickname}, I'm a chat bot.  See what I can do by asking for **/help**.".format(
-        nickname=sender.firstName
-    )
+    response.markdown = "Hello {}, I'm a chat bot. ".format(sender.firstName)
+    response.markdown += "See what I can do by asking for **/help**."
     return response
 
 
+# Create functions that will be linked to bot commands to add capabilities
+# ------------------------------------------------------------------------
+
+# A simple command that returns a basic string that will be sent as a reply
 def do_something(incoming_msg):
     """
     Sample function to do some action.
@@ -36,33 +50,43 @@ def do_something(incoming_msg):
     return "i did what you said - {}".format(incoming_msg.text)
 
 
+# An example using a Response object.  Response objects allow more complex
+# replies including sending files, html, markdown, or text. Rsponse objects
+# can also set a roomId to send response to a different room from where
+# incoming message was recieved.
 def ret_message(incoming_msg):
-    m = Response()
+    """
+    Sample function that uses a Response object for more options.
+    :param incoming_msg: The incoming message object from Teams
+    :return: A Response object based reply
+    """
+    # Create a object to create a reply.
+    response = Response()
+
+    # Set the text of the reply.
+    response.text = "Here's a fun little meme."
+
+    # Craft a URL for a file to attach to message
     u = "https://sayingimages.com/wp-content/uploads/"
     u = u + "aaaaaalll-righty-then-alrighty-meme.jpg"
-    m.files = u
-    return m
+    response.files = u
+    return response
 
 
-# Create a new bot
-bot = TeamsBot(
-    bot_app_name,
-    teams_bot_token=teams_token,
-    teams_bot_url=bot_url,
-    teams_bot_email=bot_email,
-    debug=True,
-)
-
-# Change the bot greeting
+# Set the bot greeting.
 bot.set_greeting(greeting)
 
-# Add new command
+# Add new commands to the box.
 bot.add_command("/dosomething", "help for do something", do_something)
 bot.add_command(
-    "/demo",
-    "sample that allows Webex Teams message to be returned",
-    ret_message,
+    "/demo", "Sample that creates a Teams message to be returned.", ret_message
 )
 
-# Run Bot
-bot.run(host="0.0.0.0", port=5000)
+# Every bot includes a default "/echo" command.  You can remove it, or any
+# other command with the remove_command(command) method.
+bot.remove_command("/echo")
+
+
+if __name__ == "__main__":
+    # Run Bot
+    bot.run(host="0.0.0.0", port=5000)
